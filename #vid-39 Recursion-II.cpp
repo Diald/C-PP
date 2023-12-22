141b02d87f5006903a79bcd893e50737b3d8adf3
Divya Gambhir <114723756+Diald@users.noreply.github.com>
2023-12-22 14:40:12 +0530

    Recursion

--- #vid-39 Recursion-II.cpp
+++ #vid-39 Recursion-II.cpp
@@ -21,6 +21,27 @@ int inc(int n){
     inc(n-1);
     cout<<n;
 }
+// to find first and last occurance of an element 
+int firstOcc(int arr[],int n, int i, int key){
+    if(i==n){
+        return -1;}
+    if(arr[i] == key){
+        return i;}
+    firstOcc(arr,n,0,i+1,key);
+}
+int lastOcc(int arr[], int n, int i, int key){
+    int a = lastOcc(arr,n,i+1,key);
+    if(i==n){
+        return -1;
+    }
+    if(arr[i]==key){
+        return i;
+    }
+    if(a!=-1){
+        return a;
+    }
+    return -1;
+}
 int main(){
     int arr[]={1,2,3,6,5};
     cout<< issorted(arr,5)<<"\n";
